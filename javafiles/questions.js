e85442c11cc07a900e8ba2b9d583055ef3ae049f
Snookie <kieransnook35@gmail.com>
2020-03-11 19:02:00 -0400

    got questions to rotate as well as answers

--- javafiles/questions.js
+++ javafiles/questions.js
@@ -1,41 +1,44 @@
-var questions = [{
-
+var questions = 
+[
+  {
     question: "What does D.O.M. stand for?",
-            ans1: "Direct Obsessive Messages",
-            ans2: "Document Object Model",
-            ans3: "Debby Over Mike",
-            ans4: "Document On Mozilla",
-            correctans: "1"
-    }, 
-    {
+    ans1: "Direct Obsessive Messages",
+    ans2: "Document Object Model",
+    ans3: "Debby Over Mike",
+    ans4: "Document On Mozilla",
+    correctans: "2"
+  },
+  {
     question: "How do you call Javascript code?",
-        ans1: "Call java",
-        ans2: "<script>",
-        ans3: "Function/method",
-        ans4: "Use Javascript",
-        correctans:  "2"
-    },
-    {
-         question: "________________ describes how certain HTML elements are displayed on a page",
-                ans1: "Cascading Style Sheets",
-                ans2: "Computer Screen Style",
-                ans3: "Crazy Html Style",
-                ans4:  "Style",
-                correctans:"0",
-    },
-    {
-        question: "Which HTML attribute is used to define inline styles?",
-                ans1: "font",
-                ans2: "class",
-                ans3: "styles",
-                ans4: "style",
-                correctans: "3"
-        },
-        {
-        question: "Correct HTML tag for the largest heading is:",
-                ans1: "<head>",
-                ans2: "<h6>",
-                ans3: "<heading>",
-                ans4: "<h1>",
-                correctans: "3"
-}]
+    ans1: "<Call java>",
+    ans2: "<script>",
+    ans3: "Function/method",
+    ans4: "Use Javascript",
+    correctans: "2"
+  },
+  {
+    question:
+      ".________________ describes how certain HTML elements are displayed on a page",
+    ans1: "Cascading Style Sheets",
+    ans2: "Computer Screen Style",
+    ans3: "Crazy Html Style",
+    ans4: "Style",
+    correctans: "1"
+  },
+  {
+    question: "Which HTML attribute is used to define inline styles?",
+    ans1: "font",
+    ans2: "class",
+    ans3: "styles",
+    ans4: "style",
+    correctans: "3"
+  },
+  {
+    question: "Correct HTML tag for the largest heading is:",
+    ans1: "<head>",
+    ans2: "<h6>",
+    ans3: "<heading>",
+    ans4: "<h1>",
+    correctans: "4"
+  }
+];
--- javafiles/script.js
+++ javafiles/script.js
@@ -3,28 +3,106 @@
 var currentQ = 0;
 var score = 0;
 var c = 70;
-var totalQ = questions.length;
+var questionEl = document.getElementById("question");
+var ans1 = document.getElementById("ans1");
+var ans2 = document.getElementById("ans2");
+var ans3 = document.getElementById("ans3");
+var ans4 = document.getElementById("ans4");
+var questionIndex = 0;
+var nextBtn = document.getElementById("next");
+var startBtn = document.getElementById("start");
+var subBtn = document.getElementById("submit");
+var result = document.getElementById("result");
 
+// var totquestions = questions.length;
 
+function loadquestion() {
+  document.getElementById("start").disabled = true;
+  // var qI = questions
 
-var myTimer = setInterval(myClock, 1000);
+  questionEl.textContent = questions[questionIndex].question;
+  ans1.textContent = questions[questionIndex].ans1;
+  ans2.textContent = questions[questionIndex].ans2;
+  ans3.textContent = questions[questionIndex].ans3;
+  ans4.textContent = questions[questionIndex].ans4;
+}
+function loadNextQuestion() {
+  questionEl.textContent = questions[questionIndex++];
+  questionEl.textContent = questions[questionIndex].question;
+  ans1.textContent = questions[questionIndex].ans1;
+  ans2.textContent = questions[questionIndex].ans2;
+  ans3.textContent = questions[questionIndex].ans3;
+  ans4.textContent = questions[questionIndex].ans4;
 
+  var userChoice = document.querySelector(
+    "input[type=radio]:checked"
+  );
+  if (!userChoice) {
+    alert("Please select answer.");
+    return;
+  }
+  var answer = userChoice.value;
+  if (questions[currentQ].correctans === answer) {
+    alert("congrats you got it right!");
+  }
+  userChoice.checked = false;
+  currentQ++;
+  // if (currentQ == totquestions){
+  //     startBtn.style.visibility='hidden';
+  //     nextBtn.style.visibility='hidden';
+  //     quizContainer.style.display="none";
+  //     result.style.display="";
+  //     result.textContent = score;
+  return;
+}
 
-
-//Questions
-        
-        
+// function loadNextQuestion(){
+//   var userChoice = document.querySelector('input[type=radio]:checked');
+//   if(!userChoice){
+//       alert("Please select answer.");
+//       return;
+//   }
+//       var answer = userChoice.value;
+//  if(questions[currentQ].answer === answer){
+//     score += 1;
+//  }}
+// function loadNextQuestion(){
+// questionEl.textContent = questions[questionIndex+1].question;
+//   ans1.textContent = questions[questionIndex+1].ans1;
+//   ans2.textContent = questions[questionIndex+1].ans2;
+//   ans3.textContent = questions[questionIndex+1].ans3;
+//   ans4.textContent = questions[questionIndex+1].ans4;
+// };
+// function loadNextQuestion(){
+// questionEl.textContent = questions[questionIndex+2].question;
+//     ans1.textContent = questions[questionIndex+2].ans1;
+//     ans2.textContent = questions[questionIndex+2].ans2;
+//     ans3.textContent = questions[questionIndex+2].ans3;
+//     ans4.textContent = questions[questionIndex+2].ans4;
+//   };
+// function loadNextQuestion(){
+// questionEl.textContent = questions[questionIndex+3].question;
+//       ans1.textContent = questions[questionIndex+3].ans1;
+//       ans2.textContent = questions[questionIndex+3].ans2;
+//       ans3.textContent = questions[questionIndex+3].ans3;
+//       ans4.textContent = questions[questionIndex+3].ans4;
+//     };
+// function loadNextQuestion(){
+//   questionEl.textContent = questions[questionIndex+4].question;
+//     ans1.textContent = questions[questionIndex+4].ans1;
+//     ans2.textContent = questions[questionIndex+4].ans2;
+//     ans3.textContent = questions[questionIndex+4].ans3;
+//     ans4.textContent = questions[questionIndex+4].ans4;
+//   };
 
 //Stopwatch
-var myTimer;
-        function clock() {
-                myTimer = setInterval(myClock, 1000);
-function myClock() {
-        document.getElementById("timer").innerHTML = c--;
-        if (c == 0) {
-        clearInterval(myTimer);
-        }
-}
-}
 
-
+function clock() {
+  var myTimer = setInterval(myClock, 1000);
+  function myClock() {
+    document.getElementById("timer").innerHTML = c--;
+    if (c == 0) {
+      clearInterval(myTimer);
+    }
+  }
+}
